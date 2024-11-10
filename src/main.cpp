4aa178045aa4b3a249d79e806c9b6dda18495cc1
Michael Braun <michael.braun1994@gmail.com>
2024-11-10 12:09:03 +0100

    Add clang-format support.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+
 #include "lib.hpp"
 
-int main(int argc, char** argv) {
+int main()
+{
   std::cout << "Hello world!" << std::endl;
+  int foobar = 3;
+
+  if (foobar > 3)
+  {
+    foobar = 4;
+  }
+  return 1;
 
   foo();
   return 0;
-
 }
