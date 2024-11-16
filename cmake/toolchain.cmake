61c8ef26144984ede4c3ac12148e24d1a35f6859
Michael Braun <michael.braun1994@gmail.com>
2024-11-16 21:07:13 +0000

    Add toolchain file for devcontainer.

---
+++ cmake/toolchain.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_C_COMPILER /usr/bin/clang)
+set(CMAKE_CXX_COMPILER /usr/bin/clang++)
+
+#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-arcs -ftest-coverage -fno-elide-constructors -fprofile-instr-generate -fcoverage-mapping")
+#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fprofile-arcs -ftest-coverage -fprofile-instr-generate -fcoverage-mapping")
